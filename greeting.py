fa6e0fd1868de49e11a798c92f6fac1bc59202d9
Asabeneh <asabeneh@gmail.com>
2017-01-28 00:04:45 +0200

    Commenting each line

--- greeting.py
+++ greeting.py
@@ -1,17 +1,42 @@
 name =input("What is your name: ").lower()
 lang = input("What language do you speak: ").lower()
-live = input("Where do you live: ").lower()
+live = input("Where do you live(England,Spain, France,Finland, Ethiopia): ").lower()
 def greet(lang,name,live):
     if(lang == 'eng'):
-        print("Hello",name)
+        if(live =="England"):
+            print("Hello",name)
+            print("You may speak English fluently")
+        else:
+            print("Hello",name)
+            print("Your are proficient English user.")
     elif(lang=='es'):
-        print("Hola",name)
+        if(live =="Spain"):
+            print("Holla",name)
+            print("You may speak Spanish fluently")
+        else:
+            print("Hello",name)
+            print("Your are proficient Spanish user.")
     elif(lang=='fr'):
-        print("Bonjour",name)
+        if(live =="France"):
+            print("Bonjour",name)
+            print("You may speak French fluently")
+        else:
+            print("Hello",name)
+            print("Your are proficient English user.")
     elif(lang=='fi'):
-        print("Moi",name)
+        if(live =="Finland"):
+            print("Moi",name)
+            print("You may speak Finnish fluently")
+        else:
+            print("Moi",name)
+            print("Your are proficient Finnish user.")
     elif(lang=='am'):
-        print("Selam",name)
+        if(live =="Ethiopia"):
+            print("Selam",name)
+            print("You may speak Amharic fluently")
+        else:
+            print("Hello",name)
+            print("Your are proficient Amharic user.")
     else:
         print("Do you speak English?",name)
 greet(lang,name,live)
