4ca86adf63cccb05209ff09e7c001431f80b22b6
Asabeneh <asabeneh@gmail.com>
2017-01-28 00:08:44 +0200

    Commenting

--- fileProcessing.py
+++ fileProcessing.py
@@ -1,23 +1,28 @@
+# Instruction for the user to put the proper file format
 print("Enter mbox.txt or mbox-short.txt!")
+#Geting the file, the file is in the directory
 fname = input("Enter file name in .txt form: ")
+#To handle  errors gracefully
 try:
-    fopen = open(fname)
+    fopen = open(fname) # opening file
 except:
     print("File can't be opened: ",fname)
     exit()
+#Creating a python list or arrays in other programming language for instace in C and JavaScript
 emails = []
-counter = 0
-numline = 0
+counter = 0 #Counts the number of lines which contain email
+numline = 0 #Counts the totla number of lines in the file
+#Looping through the file line by line
 for line in fopen:
     numline+=1
-    line = line.rstrip()
-    if(line.startswith('From ')):
-        spos = line.find(' ')
+    line = line.rstrip() # removing white space in both sides of the line
+    if(line.startswith('From ')):#Filtering the lines having email
+        spos = line.find(' ') #The beginning of the email string positon after removing from
         emailline= line[spos:].lstrip()
-        epos = emailline.find(' ')
-        email = emailline[0:epos]
+        epos = emailline.find(' ') # space after email, end position
+        email = emailline[0:epos] #slicing out the email
         counter =counter + 1
-        emails.insert(counter,email)
+        emails.insert(counter,email) #inserting each emails in the list
         email = emailline[0:epos]
 
 print("\nE-mails in python list: \n", emails)
