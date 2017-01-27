b246664165c97635ca5725bd339f8a1dcb0cea3e
Asabeneh <asabeneh@gmail.com>
2017-01-28 01:12:09 +0200

    modified

---
+++ pyforloop.py
@@ -0,0 +1,10 @@
+import math
+for i in range(100):
+    if(i%4 == 0):
+        print (i**2)
+fruits = ['banan', 'orange','manago','papaya','apple']
+for fruit in fruits:
+    if(fruit == 'apple'):
+        print ("I am alergic to ",fruit)
+    else:
+        print("I really love ",fruit)
