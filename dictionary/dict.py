0f1ebf141453ff06f9bed17a6d2b40fbb8faa3f2
Asabeneh <asabeneh@gmail.com>
2017-01-28 09:27:56 +0200

    modified

---
+++ dictionary/dict.py
@@ -0,0 +1,12 @@
+shoppingcart = dict()
+shoppingcart['milk'] = 3
+shoppingcart['yoghurt'] = 2
+shoppingcart['coffe'] = 1
+shoppingcart['potato'] = 2
+shoppingcart['tomato'] = 2
+shoppingcart['banana'] = 2.5
+print(shoppingcart)
+print(dir(shoppingcart))
+print(shoppingcart.items())
+print(shoppingcart.popitem())
+print(shoppingcart.pop(0))
