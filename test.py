d5b378a267557e338eab2274e3a878edb736e089
Asabeneh <asabeneh@gmail.com>
2017-01-28 07:01:12 +0200

    change to all

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 test = input('What is your name:')
 if(test == "Asabeneh"):
-    print(test,"studies computers sceince.")
-elif(test =="Befekadu"):
-    print(test,"studies computers sceince.")
+    print(test,"studies computer sceince.")
+elif(test =="Robert"):
+    print(test,"studies Information Technology.")
 elif(test =="Alem"):
     print(test,"he is a lawyer.")
 elif(test =="Rosa"):
     print(test,"lives in Ethiopia.")
 else:
-    print("I don know him")
+    print("I don know him, he/she may be a ninja")
